202de7facdb846c0957a0c3c0ff86fca5d522061
Kairokas <kairo9@tlu.ee>
2023-04-05 19:37:46 +0300

    Main menu soundtrack added

--- griZZly_live/Assets/Scripts/MainMenu.cs
+++ griZZly_live/Assets/Scripts/MainMenu.cs
@@ -5,10 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private GameObject backgroundMusic;
+
+    private void Start() {
+        backgroundMusic = GameObject.Find("BackgroundMusic");
+    }
+
     public void PlayGame ()
     {
+        StartCoroutine(FadeOut(backgroundMusic.GetComponent<AudioSource>(), 1, gameObject));
+    }
+
+    public static IEnumerator FadeOut(AudioSource audioSource, float FadeTime, GameObject mainMenuObject) {
+        float startVolume = audioSource.volume;
+ 
+        while (audioSource.volume > 0) {
+            audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
+ 
+            yield return null;
+        }
+ 
+        audioSource.Stop ();
+        audioSource.volume = startVolume;
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        mainMenuObject.SetActive(false);
     }
 
     public void QuitGame ()
