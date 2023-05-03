32e3a3e3e5e5a52927cfa7bd1716dbc6b0900b1d
Kaius Karon <kaiusk@tlu.ee>
2023-05-03 08:31:33 +0300

    kaamera offset

--- griZZly_live/Assets/Scripts/CameraManager.cs
+++ griZZly_live/Assets/Scripts/CameraManager.cs
@@ -6,60 +6,68 @@ public class CameraManager : MonoBehaviour {
     public static CameraManager Instance;
     public CinemachineVirtualCamera VMcam;
     public GameObject player;
+    public float PlayerOffset;
+    public float EnemyOffset;
     public GameObject enemy;
     public GameObject ammo;
-    
+
     public float introDuration = 5;
     private bool canChange;
 
     private float orthographicSize = 15f;
+    private CinemachineFramingTransposer transposer;
     float t = 0;
-   private bool startMovement = false;
-    
-    
+    private bool startMovement = false;
+
+
     public void Awake() {
         Instance = this;
         GameManager.OnGameStateChanged += GameManagerOnGameStateChanged;
+        transposer = VMcam.GetCinemachineComponent<CinemachineFramingTransposer>();
     }
 
     public void OnDestroy() {
-        if (GameManager.Instance)
-        {
+        if (GameManager.Instance) {
             GameManager.OnGameStateChanged -= GameManagerOnGameStateChanged;
         }
     }
 
     public void GameManagerOnGameStateChanged(GameState state) {
-        if (state == GameState.Intro) {
-
-        } else if (state == GameState.PlayerTurn) {
+        if (state == GameState.Intro) { }
+        else if (state == GameState.PlayerTurn) {
             VMcam.Follow = player.transform;
             VMcam.m_Lens.OrthographicSize = 4f;
-            //VMcam.m_Follow.
-        } else if (state == GameState.EnemyTurn) {
-            VMcam.Follow = enemy.transform;    
-        } else if (state == GameState.FallowAmmo1) {
+            transposer.m_TrackedObjectOffset = new Vector3(PlayerOffset, 0, 0);
+
+        }
+        else if (state == GameState.EnemyTurn) {
+            VMcam.Follow = enemy.transform;
+            transposer.m_TrackedObjectOffset = new Vector3(EnemyOffset, 0, 0);
+        }
+        else if (state == GameState.FallowAmmo1) {
             canChange = true;
             VMcam.Follow = ammo.transform;
-        } else if (state == GameState.FallowAmmo2) {
+            transposer.m_TrackedObjectOffset = new Vector3(0, 0, 0);
+        }
+        else if (state == GameState.FallowAmmo2) {
             canChange = true;
             VMcam.Follow = GameObject.FindWithTag("Stone").transform;
-        } 
+            transposer.m_TrackedObjectOffset = new Vector3(0, 0, 0);
+        }
     }
-    
-    void LateUpdate()
-    {
+
+    void LateUpdate() {
         if (GameManager.Instance.State == GameState.FallowAmmo1 && VMcam.transform.position.x > 22 && canChange) {
             VMcam.Follow = enemy.transform;
             canChange = false;
         }
-        
+
         if (GameManager.Instance.State == GameState.FallowAmmo2 && VMcam.transform.position.x < -1 && canChange) {
             VMcam.Follow = player.transform;
             canChange = false;
         }
 
-        if (GameManager.Instance.State == GameState.Intro ) {
+        if (GameManager.Instance.State == GameState.Intro) {
             StartCoroutine(enableMovement());
             if (startMovement) {
                 t += Time.deltaTime / introDuration;
@@ -67,17 +75,15 @@ public class CameraManager : MonoBehaviour {
                 VMcam.m_Lens.OrthographicSize = orthographicSize;
                 VMcam.transform.position =
                     Vector3.Lerp(
-                        new Vector3(7.9f, 7.3f, -10f), 
-                        new Vector3(-11.6f, -1.8f, -10f), 
+                        new Vector3(7.9f, 7.3f, -10f),
+                        new Vector3(-11.6f, -1.8f, -10f),
                         t); // move to babushka
             }
         }
-        
-        IEnumerator enableMovement()
-        {
+
+        IEnumerator enableMovement() {
             yield return new WaitForSeconds(3);
             startMovement = true;
         }
-
     }
 }
